f06c7bba3ec214720f0bae1e280c508f27a88537
j9recurses <j9.heiser@ischool.berkeley.edu>
2015-09-14 07:45:44 -0700

    factory plus tweaks on functions

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,10 +9,15 @@ angular.module('calculatorApp')
       $scope.operator = null //set the operator to empty; will trigger the doTheMath
                             //function when its not null to run the math operations
 
-      $screenReset = true; //stores screen state; tells us whether or not
-                          //we should store/register digits that a user is
-                          //inputting
+      //a flag for clearing the output screen; tells us whether or not
+       //we should store/register digits that a user is inputting into the keypad
+      $scope.screenReset = true;
 
+      $scope.decimalClick = false; //use to record if a decimal click has occured.
+
+       //used to determine if a user entered a decimal number
+       //that has more than one decimal places
+      $scope.decimalClickDouble = false;
       $scope.ac = {label: "AC", value: true}
 
       $scope.digits = [
@@ -34,12 +39,18 @@ angular.module('calculatorApp')
       $scope.numTransforms = [
         {label: "%", operation: CalculatorFactory.percentage },
         {label: "+/-", operation: CalculatorFactory.posNeg },
-        {label:".", operation: CalculatorFactory. makeDecimal}
+        {label:"."}
       ];
 
-      //function to reset the screen/the result val
+      //function to reset the screen and calculator
       $scope.clearAll = function (digit) {
         $scope.result = 0;
+        $scope.screenReset = true;
+        $scope.entered1 = 0;
+        $scope.entered2 = 0;
+        $scope.operator = null ;
+        $scope.decimalClick = false;
+        $scope.decimalClickDouble = false;
       }
 
       //function to handle digit clicks;
@@ -51,50 +62,85 @@ angular.module('calculatorApp')
           //after a user enters a number, we need to
           ///we set the screenReset val to false so
           ///that we know that there are numbers in the display
-          $scope.screenReset  = false;
+           $scope.screenReset  = false;
+        }
+
+        //run decimal conversion after 2 numbers have
+        //been inputed and decimal button click event
+        else if ($scope.decimalClick){
+          if($scope.decimalClickDouble){
+
+            //after 1 decimal place dit has been entered
+            //need to divide the result by base
+            //10 every time a decimal digit is entered
+            //($scope.entered2 / 10) + digit;
+             $scope.result = CalculatorFactory.digitNPlusOneDecimal($scope.entered2, digit)
+          }else{
+            $scope.decimalClickDouble = true;
+            $scope.result =  CalculatorFactory.digitfirstDecimal($scope.entered2, digit)
+            //$scope.entered2 + (digit/10);
+          }
         }
         else {
           //We need to make room  in the display
-          //for the nth/last digit entered.
+          //for the nth/last digit entered. ie "1" "2" "3"
+          //button clicks = the number 1,2,3
           //So we can move the n-1 digit entered over to the left
           //by scaling the current display value by base 10
           //and then adding the second digit entered.
           //also need to save digit of the second item entered for
           //the math operation fxn
-          $scope.result = $scope.result * 10 + digit;
+          $scope.result = CalculatorFactory.digitBase10($scope.result, digit);
+
         }
           //store the val of the number entered;
           //we will need this later to run the operator function
-          $scope.entered2 = $scope.result
+          $scope.entered2 = $scope.result;
       };
 
       //called when a user clicks +,-, x, / buttons
       $scope.operatorClicked = function (operator) {
-
-        //stores the operator function that the user
-        //clicked on; will actually run the operation
-        //when the user clicks on the equals= button
+       //check see if there an operation already stored
+       //if exists, show the result to the user, then accept
+       //the new math operator
+        if($scope.operation){
+           $scope.doTheMath();
+        }
+        //stores the operator that the user clicked on
         $scope.operation = operator;
-
         //store the val of the first number that the user inputted.
         $scope.entered1 = $scope.result;
         $scope.entered2 = $scope.result;
         //after a math operation is clicked/entered, user will
         //need to enter a second number to run the operation on.
         //Set the screenReset var to true so we can store/get
-        //new digits that user is clicking on.
-        $scope.screenReset = true;
+        //new digits that the user is clicking on.
+         $scope.screenReset = true;
+         ///clear out the decimal click bools
+         $scope.decimalClick = false;
+         $scope.decimalClickDouble = false;
       };
 
-      //called when the user clicks equals button. Runs the operation and
-      //displays the result in the view.
-      $scope.doTheMath = function () {
-        if($scope.operation != null) {
 
+      //Runs the given operation on 2 digit values and
+      //displays the result in the view.
+      $scope.doTheMath =  function doTheMath() {
+        if($scope.operation != null) {
           //update the view with the result from math operation on 2 numbers
           $scope.result = $scope.operation($scope.entered1, $scope.entered2);
-          //after displaying result,
+          //after displaying result, don't need to keep
+          //track of digit keys entered
           $scope.screenReset = true;
+
+          //clear the operation that was just run
+          $scope.operation = null;
+
+          //clear the decimal flags
+          $scope.decimalClick = false;
+          $scope.decimalClickDouble = false;
+
+          //saves the current result as the first val
+          //for the next operation
           $scope.entered1 = $scope.result;
         }
       };
@@ -107,6 +153,13 @@ angular.module('calculatorApp')
           //use it later to run the operator function
           $scope.entered2 = $scope.result;
       };
+
+      $scope.decimalClicked = function(operator){
+        $scope.decimaloperator = operator;
+        $scope.decimalClick = true;
+        $scope.result = $scope.result + "."
+      };
+
 
   }]);
 
--- public/js/services.js
+++ public/js/services.js
@@ -25,12 +25,25 @@ angular.module('calculatorApp').factory('CalculatorFactory', function(){
           }
 
         };
-        var makeDecimal = function(a, b){
-           return (a.toFixed(1) + percentage(b));
+        var digitfirstDecimal = function(a,b){
+          return a + (b/10);
         };
+        var digitNPlusOneDecimal = function(a, b){
+            console.log(a.toString().split(".")[0]);
+            console.log(a.toString().split(".")[1]);
+            before =  a.toString().split(".")[0];
+            after =   a.toString().split(".")[1];
+            after =   after +b ;
+            return parseFloat(before + "." + after);
+        };
+       var digitBase10 = function(a, b){
+          return a * 10 + b;
+        };
+
         return {
           add: add, subtract :subtract , multiply:multiply, divide:divide,
-          percentage:percentage, posNeg:posNeg, makeDecimal: makeDecimal
+          percentage:percentage, posNeg:posNeg, digitfirstDecimal: digitfirstDecimal,
+          digitNPlusOneDecimal: digitNPlusOneDecimal,digitBase10:digitBase10
         };
 });
 
